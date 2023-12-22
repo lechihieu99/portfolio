7f2490720e24e2cb6c6ddf1cb65670bc3b172b6e
lechihieu99 <lechihieu99us@gmail.com>
2023-12-22 08:25:56 +0700

    change url

--- src/components/section5/Section5.jsx
+++ src/components/section5/Section5.jsx
@@ -38,7 +38,7 @@ const Section5 = ({ animate }) => {
                 fullname: name,
                 message: message
             }
-            axios.post('https://8670-2401-d800-8fa-7c4f-150-bf3e-ec69-8b18.ngrok-free.app/api/send-email', payload)
+            axios.post('https://0cc5-2401-d800-2b23-3fcb-e9c8-c2ab-8f4b-efb7.ngrok-free.app/api/send-email', payload)
 
             setMess(t('section5.messSuccess'))
             setTimeout(() => {
